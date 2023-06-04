f99ec9d01ac163dafa326add8a60ffc1472f20c5
LizGuile <lizguile19@gmail.com>
2023-06-04 15:03:52 +0800

    practice integer types, desk

---
+++ Topics/Integer types and operations/Desks/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int inputNum1 = scanner.nextInt();
+        int inputNum2 = scanner.nextInt();
+        int inputNum3 = scanner.nextInt();
+        int result = inputNum1/2 + inputNum2/2 + inputNum3/2
+                + inputNum1%2 + inputNum2%2 + inputNum3%2;
+        System.out.print(result);
+    }
+}
